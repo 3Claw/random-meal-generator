fe9db07b8206536a0605a6e4fa44691b36fa07b3
Arso Adhitio <70020996+3Claw@users.noreply.github.com>
2021-02-08 13:37:15 +0700

    Add files via upload

--- random_meal_generator/js/main.js
+++ random_meal_generator/js/main.js
@@ -12,25 +12,38 @@ get_meal_btn.addEventListener('click', () => {
         });
 });
 
-const createMeal = (meal) => {
-    const ingredients = [];
-    for(let i=1; i<=20; i++) {
-        if(meal [`strIngredient${i}`]) {
-            ingredients.push(`${meal[`strIngredient`]} - ${meal[`strMeasure${i}`]}`)
-        } else {
-            // Stop if no more ingredients
-        }
-            break;
-        }
-    }
+const createMeal = meal => {
+	const ingredients = [];
 
-const newInnerHTML = `
+	// Get all ingredients from the object. Up to 20
+	for (let i = 1; i <= 20; i++) {
+		if (meal[`strIngredient${i}`]) {
+			ingredients.push(
+				`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+			);
+		} else {
+			// Stop if there are no more ingredients
+			break;
+		}
+	}
+
+	const newInnerHTML = `
 	<div class="row">
 		<div class="columns five">
 			<img src="${meal.strMealThumb}" alt="Meal Image">
-			${meal.strCategory ? `<p><strong>Category:</strong> ${meal.strCategory}</p>` : ''}
+			${
+				meal.strCategory
+					? `<p><strong>Category:</strong> ${meal.strCategory}</p>`
+					: ''
+			}
 			${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ''}
-			${meal.strTags ? `<p><strong>Tags:</strong> ${meal.strTags.split(',').join(', ')}</p>` : ''}
+			${
+				meal.strTags
+					? `<p><strong>Tags:</strong> ${meal.strTags
+							.split(',')
+							.join(', ')}</p>`
+					: ''
+			}
 			<h5>Ingredients:</h5>
 			<ul>
 				${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
@@ -41,18 +54,23 @@ const newInnerHTML = `
 			<p>${meal.strInstructions}</p>
 		</div>
 	</div>
-		${meal.strYoutube ? `
-		<div class="row">
-			<h5>Video Recipe</h5>
-			<div class="videoWrapper">
-				<iframe width="420" height="315"
-				src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
-				</iframe>
-			</div>
-		</div>` : ''}
+	${
+		meal.strYoutube
+			? `
+	<div class="row">
+		<h5>Video Recipe</h5>
+		<div class="videoWrapper">
+			<iframe width="420" height="315"
+			src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
+			</iframe>
+		</div>
+	</div>`
+			: ''
+	}
 `;
 
 meal_container.innerHTML = newInnerHTML;
+};
 
 
 // Social panel js
